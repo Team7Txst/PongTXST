f7aab86a4066e8d4c81eb179be4df3afa0c4179f
Sean Summers <sgs79@txstate.edu>
2020-08-01 19:38:31 -0500

    Pong is running
    updated main file and deleted files we no longer need

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,22 @@
 package sample;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 import java.util.Random;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.TextAlignment;
+import javafx.stage.Stage;
 import javafx.util.Duration;
-
 
 public class Main extends Application {
 
-    //Variables and Decelerations
+    //variable
     private static final int width = 800;
     private static final int height = 600;
     private static final int PLAYER_HEIGHT = 100;
@@ -30,7 +27,7 @@ public class Main extends Application {
     private double playerOneYPos = height / 2;
     private double playerTwoYPos = height / 2;
     private double ballXPos = width / 2;
-    private double ballYPos = width / 2;
+    private double ballYPos = height / 2;
     private int scoreP1 = 0;
     private int scoreP2 = 0;
     private boolean gameStarted;
@@ -38,30 +35,95 @@ public class Main extends Application {
     private double playerTwoXPos = width - PLAYER_WIDTH;
 
     public void start(Stage stage) throws Exception {
-        stage.setTitle("NOT PONG 4 CW PURPOSES");
+        stage.setTitle("Not Pong 4 CW reasons");
+        //background size
         Canvas canvas = new Canvas(width, height);
         GraphicsContext gc = canvas.getGraphicsContext2D();
-        Timeline t1 = new Timeline(new KeyFrame(Duration.millis(10), e ->run(gc)));
-        t1.setCycleCount(Timeline.INDEFINITE);
 
-        //Mouse Control
-        canvas.setOnMouseMoved(e -> playerOneYPos = e.getY());
-        canvas.setOnMouseClicked(e -> gameStarted = true);
+        //JavaFX Timeline = free form animation defined by KeyFrames and their duration
+        Timeline tl = new Timeline(new KeyFrame(Duration.millis(10), e -> run(gc)));
+        //number of cycles in animation INDEFINITE = repeat indefinitely
+        tl.setCycleCount(Timeline.INDEFINITE);
+
+        //mouse control (move and click)
+        canvas.setOnMouseMoved(e ->  playerOneYPos  = e.getY());
+        canvas.setOnMouseClicked(e ->  gameStarted = true);
         stage.setScene(new Scene(new StackPane(canvas)));
         stage.show();
-        t1.play();
+        tl.play();
     }
 
     private void run(GraphicsContext gc) {
-        //setting background color
+        //set graphics
+        //set background color
         gc.setFill(Color.BLACK);
         gc.fillRect(0, 0, width, height);
 
-        //setting text color
+        //set text
         gc.setFill(Color.WHITE);
         gc.setFont(Font.font(25));
+
+        if(gameStarted) {
+            //set ball movement
+            ballXPos+=ballXSpeed;
+            ballYPos+=ballYSpeed;
+
+            //simple computer opponent who is following the ball
+            if(ballXPos < width - width  / 4) {
+                playerTwoYPos = ballYPos - PLAYER_HEIGHT / 2;
+            }  else {
+                playerTwoYPos =  ballYPos > playerTwoYPos + PLAYER_HEIGHT / 2 ?playerTwoYPos += 1: playerTwoYPos - 1;
+            }
+            //draw the ball
+            gc.fillOval(ballXPos, ballYPos, BALL_R, BALL_R);
+
+        } else {
+            //set the start text
+            gc.setStroke(Color.WHITE);
+            gc.setTextAlign(TextAlignment.CENTER);
+            gc.strokeText("Click", width / 2, height / 2);
+
+            //reset the ball start position
+            ballXPos = width / 2;
+            ballYPos = height / 2;
+
+            //reset the ball speed and the direction
+            ballXSpeed = new Random().nextInt(2) == 0 ? 1: -1;
+            ballYSpeed = new Random().nextInt(2) == 0 ? 1: -1;
+        }
+
+        //makes sure the ball stays in the canvas
+        if(ballYPos > height || ballYPos < 0) ballYSpeed *=-1;
+
+        //if you miss the ball, computer gets a point
+        if(ballXPos < playerOneXPos - PLAYER_WIDTH) {
+            scoreP2++;
+            gameStarted = false;
+        }
+
+        //if the computer misses the ball, you get a point
+        if(ballXPos > playerTwoXPos + PLAYER_WIDTH) {
+            scoreP1++;
+            gameStarted = false;
+        }
+
+        //increase the speed after the ball hits the player
+        if( ((ballXPos + BALL_R > playerTwoXPos) && ballYPos >= playerTwoYPos && ballYPos <= playerTwoYPos + PLAYER_HEIGHT) ||
+                ((ballXPos < playerOneXPos + PLAYER_WIDTH) && ballYPos >= playerOneYPos && ballYPos <= playerOneYPos + PLAYER_HEIGHT)) {
+            ballYSpeed += 1 * Math.signum(ballYSpeed);
+            ballXSpeed += 1 * Math.signum(ballXSpeed);
+            ballXSpeed *= -1;
+            ballYSpeed *= -1;
+        }
+
+        //draw score
+        gc.fillText(scoreP1 + "\t\t\t\t\t\t\t\t" + scoreP2, width / 2, 100);
+        //draw player 1 & 2
+        gc.fillRect(playerTwoXPos, playerTwoYPos, PLAYER_WIDTH, PLAYER_HEIGHT);
+        gc.fillRect(playerOneXPos, playerOneYPos, PLAYER_WIDTH, PLAYER_HEIGHT);
     }
 
+    // start the application
     public static void main(String[] args) {
         launch(args);
     }
--- src/Paddle.java
+++
@@ -1,9 +0,0 @@
-package pong;
-
-public class Paddle {
-	
-	public void draw(Graphics g);
-	public void move();
-	public int getY();
-	
-}
