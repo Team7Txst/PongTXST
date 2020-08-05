61a90f4f9dbd768361907432e58f226c7e3125df
Sean Summers <sgs79@txstate.edu>
2020-08-05 14:35:47 -0500

    Added Multiplayer
    Added the multiplayer class and renamed the singlePlayer class to SInglePlayer

--- pong2.0/src/team7/SinglePlayer.java
+++ pong2.0/src/team7/SinglePlayer.java
@@ -14,7 +14,7 @@ import javafx.scene.text.Font;
 import javafx.scene.text.TextAlignment;
 import javafx.util.Duration;
 
-public class singlePlayer extends Application {
+public class SinglePlayer extends Application {
 
     //variable
     private static final int width = 800;
--- pong2.0/src/team7/controllers/Controller.java
+++ pong2.0/src/team7/controllers/Controller.java
@@ -1,7 +1,4 @@
 package team7.controllers;
 
-import javafx.event.Event;
-import team7.singlePlayer;
-
 public class Controller {
 }
