e63c9e55370d7d7bc4846a4a8b14554b8044e0c4
Sean Summers <sgs79@txstate.edu>
2020-08-06 16:34:57 -0500

    README commit

--- PingPongFinal/src/team7/MainWindowTest.java
+++ PingPongFinal/src/team7/MainWindowTest.java
@@ -1,21 +1,49 @@
 package team7;
 
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class MainWindowTest {
+
+    int width = 800;
+    int height = 600;
+
     @Test
-    public void startTest(){
-        MainWindow mainWindow= new MainWindow();
+    public void startTestPlayerOneYPos(){
+        MainWindow mainWindow = new MainWindow();
         double expected= 300;
         double actual= mainWindow.getPlayerOneYPos();
         Assert.assertEquals(expected, actual, 1);
     }
 
     @Test
-    public void runTest(){
+    public void startTestGameStarted(){
         MainWindow mainWindow= new MainWindow();
         boolean expected= false;
+        boolean actual= mainWindow.getGameStarted();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void runTestPlayerOneYPos(){
+        MainWindow mainWindow = new MainWindow();
+        Canvas canvas = new Canvas(width, height);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        double expected= 300;
+        mainWindow.run(gc);
+        double actual= mainWindow.getPlayerOneYPos();
+        Assert.assertEquals(expected, actual, 1);
+    }
+
+    @Test
+    public void runTestGameStarted(){
+        MainWindow mainWindow= new MainWindow();
+        Canvas canvas = new Canvas(width, height);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        boolean expected= false;
+        mainWindow.run(gc);
         boolean actual= mainWindow.getGameStarted();
         Assert.assertEquals(expected, actual);
     }
