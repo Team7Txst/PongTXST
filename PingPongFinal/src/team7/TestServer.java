2dc26d6cc17a66bf0e48d423a935017702f739a9
Sean Summers <sgs79@txstate.edu>
2020-08-06 16:37:01 -0500

    Updating Client and TestServer

--- PingPongFinal/src/team7/TestServer.java
+++ PingPongFinal/src/team7/TestServer.java
@@ -1,11 +1,12 @@
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Server
+public class TestServer
 {
-    public static void main(String[] args) throws Exception
+    //public static void main(String[] args) throws Exception
     {
         try {
             ServerSocket ss = new ServerSocket(39485); //server socket
